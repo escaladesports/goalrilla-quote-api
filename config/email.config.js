40f78b5554a734392ea34474a457c84e64c42d03
Kennedy Rose <kennedy@kennedyrose.com>
2018-03-27 10:29:40 -0500

    Changed email

--- config/email.config.js
+++ config/email.config.js
@@ -1,6 +1,5 @@
 module.exports = {
 	quoteRequestRecipients: [
-		{email: 'christian.l.sedlacek@gmail.com', name: 'Christian Sedlacek'},
-		{email: 'csedlacek@escaladesports.com', name: 'Christian Sedlacek'}
+		{email: 'krose@escaladesports.com', name: 'Kennedy Rose'},
 	]
 }
